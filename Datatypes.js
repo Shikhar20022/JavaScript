ce0dc393dbae8a3024982d63ec84764ab4e6c082
Shikhar20022 <mauryashikhar2002@gmail.com>
2023-12-17 11:44:11 +0530

    Datatype summary

---
+++ Datatypes.js
@@ -0,0 +1,37 @@
+//Data types of JavaScript
+
+// 1.Primitive
+
+// 7 types: String, Number, Boolean, null , symbol, undefined, BigInt
+
+const myName = "Shikhar"
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+// const id = symbol('123')
+
+const bigNumber = 1234576766900945624n
+
+
+// Reference (Non primitive)
+
+// Array , Object, Functions
+
+
+const heros = ["shaktiman", "nagraj", "doga",]
+
+let myObj = {
+    name: "shikhar",
+    age :  23,
+}
+
+const myFunction = function() {
+    console.log("hello world")
+}
+
+
+console.log(typeof outsideTemp)
