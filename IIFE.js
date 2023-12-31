8acd5ea22fad79553968ab52dedab594ed6215ce
Shikhar20022 <mauryashikhar2002@gmail.com>
2023-12-31 18:59:09 +0530

    IIFE

---
+++ IIFE.js
@@ -0,0 +1,18 @@
+
+
+// Immediatly invoked function expression
+
+
+(function name() {
+       console.log(`Shikhar`);
+}) ();
+
+
+
+
+( (age) => {
+      console.log(`My age is ${age}`)
+}) (18)
+
+
+
